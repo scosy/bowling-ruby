7bce4ee92fa4d6cfc418ad383741488a4b88ed00
scosy <amine.bentaleb064@gmail.com>
2022-09-12 09:20:37 +0200

    :rocket: Changed implementation and passed tests for strikes and spares

--- bowling.rb
+++ bowling.rb
@@ -1,39 +1,27 @@
+# frozen_string_literal: true
+
 class Bowling
-  @@score = 0
-  @@bonus_strike = false
-  @@bonus_spare = false
-  @@i = 0
-  @@previous_roll = 0
+  def initialize(score)
+    @score = score
+  end
 
   def roll(pins_down)
-  if @@bonus_spare
-    @@bonus_spare = false
-    roll_with_bonus(pins_down)
-    return
-  end
-
-  if @@bonus_strike
-    roll_with_bonus(pins_down)
-    @@i += 1
-    if @@i == 2
-      @@bonus_strike = false
-      @@i = 0
-    end
-    return
-  end
-
-  @@score += pins_down
-  @@bonus_strike = true if pins_down == 10
-  @@bonus_spare = true if @@previous_roll + pins_down == 10
-  @@previous_roll = pins_down
-  nil
-  end
-
-  def roll_with_bonus(pins_down)
-    @@score += pins_down * 2
+    @score << pins_down
+    nil
   end
 
   def score
-    @@score
+    @score.reverse!.map!.with_index do |n, i|
+      break if i > @score.size - 3
+
+      if @score[i + 2] == 10
+        @score[i + 1] *= 2
+        n > 10 ? n : n * 2
+      elsif @score[i + 1] + @score[i + 2] == 10
+        n * 2
+      else
+        n
+      end
+    end
   end
 end
